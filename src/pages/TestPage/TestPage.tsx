05f41fd7358f3d7e263c33e0a7ca023326e53c44
Danielfts <d.trivino@uniandes.edu.co>
2025-03-12 02:44:23 +0000

    Chore: layout

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,12 +1,14 @@
 import { FunctionComponent } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 // interface TestPageProps {}
 
 const TestPage: FunctionComponent = () => {
   return (
     <Container fluid="md">
-      <h1>Monda</h1>
+      <Row className="">
+        <h1 style={{textAlign: "center"}}>Monda</h1>
+      </Row>
     </Container>
   );
 };
